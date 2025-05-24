1e37a1146df0a18a50a933e4755b77c639ca94f5
CRYPTOOOKID <srinadhchitrakavi@gmail.com>
2025-05-24 10:13:38 +0200

    fixed typo

--- src/components/IELTS/Reading/ReadingExam.jsx
+++ src/components/IELTS/Reading/ReadingExam.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import "./ReadingExam.css";
-import ReadingResults from "./ReadingResults";
+import ReadingResults from "./Readingresults";
 import { useTimer } from "../../../lib/TimerContext";
 import TimeUpModal from "../../ui/TimeUpModal";
 import ExamContainer from "../../ui/ExamContainer";
